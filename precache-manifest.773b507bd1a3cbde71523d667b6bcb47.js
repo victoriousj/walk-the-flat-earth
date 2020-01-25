11d9a1290df0a19a778a75ce524a373ebbf92635
vic <victordjohnson@gmail.com>
2020-01-25 15:38:47 -0600

    Updates

---
+++ precache-manifest.773b507bd1a3cbde71523d667b6bcb47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880743e386a0be3c76f7a7e2528bf988",
+    "url": "/walk-the-flat-earth/index.html"
+  },
+  {
+    "revision": "b622ac5654ac08b3cc2e",
+    "url": "/walk-the-flat-earth/static/css/main.d70ae273.chunk.css"
+  },
+  {
+    "revision": "0424684c39556cc6c07e",
+    "url": "/walk-the-flat-earth/static/js/2.ca0bf4ba.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/walk-the-flat-earth/static/js/2.ca0bf4ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b622ac5654ac08b3cc2e",
+    "url": "/walk-the-flat-earth/static/js/main.83496e34.chunk.js"
+  },
+  {
+    "revision": "1ebf3a3d1be37a9708f1",
+    "url": "/walk-the-flat-earth/static/js/runtime-main.729a6eb7.js"
+  },
+  {
+    "revision": "92b82e002899058f40d9f3bae1feaa6b",
+    "url": "/walk-the-flat-earth/static/media/earth.92b82e00.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/walk-the-flat-earth/static/media/logo.5d5d9eef.svg"
+  }
+]);
